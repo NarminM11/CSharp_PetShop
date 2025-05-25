53aa56b6c9d7c0113a2d647a97c8f591ad461c72
Narmin Murshudova <nrmin.mrdova.01@bk.ru>
2025-05-25 14:54:59 +0400

    Cat class updates

---
+++ C#_PetShop/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Media;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Cat cat = new Cat("Misty", 2, 'F', 50, 200, 0.3, "Calico", 10);
+        Console.WriteLine(cat);
+
+        cat.Eat();
+        cat.Play();
+        //cat.Play();
+        //cat.Play();
+        //cat.Play();
+        //cat.Play();
+        //cat.Play();
+        //cat.Play();
+        //cat.Play();
+        Console.WriteLine(cat.Size);
+        Console.WriteLine(cat.MealQuantity);
+        Console.WriteLine(cat);
+        Console.WriteLine("Total Price: " + cat.TotalPrice());
+    }
+}
+public class PetShop
+{
+    public int[] Cats { get; set; }
+    public int[] Dogs { get; set; }
+
+    public void RemoveNickname(string nickname)
+    {
+
+    }
+}
+
+public class Pet:PetShop
+{
+    public string Nickname { get; set; }
+    public int Age { get; set; }
+    public char Gender { get; set; }
+    public double Energy { get; set; }
+    public int Price { get; set; }
+    public double MealQuantity { get; set; }
+
+    public Pet()
+    {
+        Nickname = "Unknown";
+        Age = 0;
+        Gender = 'U';
+        Energy = 0.0;
+        Price = 0;
+        MealQuantity = 0.0;
+    }
+
+    public Pet(string nickname, int age, char gender, double energy, int price, double mealQuantity)
+    {
+        Nickname = nickname;
+        Age = age;
+        Gender = gender;
+        Energy = energy;
+        Price = price;
+        MealQuantity = mealQuantity;
+    }
+
+    public virtual void Sleep() { }
+    public virtual void Eat() { }
+    public virtual void Play() { }
+
+    public override string ToString()
+    {
+        return @$"Name: {Nickname} | Age: {Age} | Gender: {Gender} | Energy: {Energy} | Price: {Price} | MealQuantity: {MealQuantity}";
+    }
+
+    public virtual int TotalPrice()
+    {
+        return Price;
+    }
+}
+
+public class Cat : Pet
+{
+    public string ColorPattern { get; set; }
+    public int Size { get; set; }
+
+    public Cat(string nickname, int age, char gender, double energy, int price, double mealQuantity, string colorPattern, int size)
+        : base(nickname, age, gender, energy, price, mealQuantity)
+    {
+        ColorPattern = colorPattern;
+        Size = size;
+    }
+
+    public override void Sleep()
+    {
+        Energy += 10;
+    }
+
+    public override void Eat()
+    {
+
+        int meal_quantity = 0;
+        string meal_name = "";
+
+        Console.WriteLine("Which food do you want the cat to eat?");
+        Console.WriteLine("1. Whiskas");
+        Console.WriteLine("2. Fish");
+        Console.WriteLine("3. Chicken");
+
+        int choice;
+        if (int.TryParse(Console.ReadLine(), out choice))
+        {
+            if (choice == 1)
+            {
+                meal_name = "Whiskas";
+            }
+            else if (choice == 2)
+            {
+                meal_name = "Fish";
+            }
+            else if (choice == 3)
+            {
+                meal_name = "Chicken";
+            }
+            else
+            {
+                Console.WriteLine("Invalid food choice.");
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
+            return;
+        }
+
+        Console.WriteLine("Please choose grams of the meal:");
+        Console.WriteLine("1. 200 gr");
+        Console.WriteLine("2. 400 gr");
+        Console.WriteLine("3. 600 gr");
+
+        int gramChoice;
+        if (int.TryParse(Console.ReadLine(), out gramChoice))
+        {
+            if (gramChoice == 1)
+            {
+                meal_quantity = 200;
+                Energy += 10;
+                Size++;
+                Price++;
+            }
+            else if (gramChoice == 2)
+            {
+                meal_quantity = 400;
+                Energy += 20;
+                Size+=2;
+                Price++;
+            }
+            else if (gramChoice == 3)
+            {
+                meal_quantity = 600;
+                Energy += 20;
+                Size+=3;
+                Price++;
+            }
+            else
+            {
+                Console.WriteLine("Invalid gram choice.");
+                return;
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Invalid input.");
+            return;
+        }
+
+        Console.WriteLine($"Cat ate {meal_quantity} gr of {meal_name} and got 10 energy.");
+        MealQuantity += (double)meal_quantity;
+
+    }
+
+    public override void Play()
+    {
+        Energy -= 10;
+        if (Energy <= 0)
+        {
+            Console.WriteLine($"{Nickname} is tired and needs to sleep.");
+            Energy = 0;
+            Sleep();
+        }
+        else
+        {
+            Console.WriteLine($"{Nickname} is playing and purring...");
+
+            SoundPlayer player = new SoundPlayer(@"C:\Users\Ferid\Downloads\181957__magnus589__fia-cat-purring.wav");
+            player.Play();
+        }
+    }
+
+    public void CalculateAge()
+    {
+        if(Size > 10)
+        {
+            Age++;
+        }
+    }
+
+    public override int TotalPrice()
+    {
+        return Price + (Size * 2);
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $" | ColorPattern: {ColorPattern} | Size: {Size}";
+    }
+
+}
+
+public class Dog : Pet
+{
+}
